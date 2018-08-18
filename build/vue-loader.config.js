a0cb418dc33c580767fa4acbea696c2d9c9de854
xiaoge <2720447804@qq.com>
2018-08-18 14:19:41 +0800

    init3

--- build/vue-loader.config.js
+++ build/vue-loader.config.js
@@ -4,7 +4,7 @@ module.exports = (isDev) => {
     extractCSS: !isDev, //vue文件里的css文件单独打包
     cssModules:{
       localIdentName: isDev ? '[path]-[name]-[hash:base64:5]' : '[hash:base64:5]',
-      camelCase:true
+      camelCase: true
     }, // 将css中-连接的类名转化成驼峰写法
     // hotReload: false // 设置false修改文件后刷新才变化
   }
